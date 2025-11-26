63d7fd1d34b3ac3bbcf5753074fa0e5b2f93580b
leartssy <121230716+leartssy@users.noreply.github.com>
2025-11-26 18:42:03 +0100

    Update pnp_style.py

--- DiffuseST_with_TexTile/pnp_style.py
+++ DiffuseST_with_TexTile/pnp_style.py
@@ -54,7 +54,7 @@ class PNP(nn.Module):
         return self.qk_injection_timesteps
     
 
-    def run_pnp(self, content_latents, style_latents, style_file, content_fn="content", style_fn="style"):
+    def run_pnp(self, content_latents, style_latents, style_file, content_fn="content", style_fn="style", opt):
         
         all_times = []
         
