499b32b7dc230cd0470d924d11330a13e12539e8
leartssy <121230716+leartssy@users.noreply.github.com>
2025-11-26 17:35:06 +0100

    Update run.py

--- DiffuseST_with_TexTile/run.py
+++ DiffuseST_with_TexTile/run.py
@@ -23,7 +23,7 @@ import textile
 #endTextileImports
 
 def run(opt):
-    model_key = "/content/drive/MyDrive/Colab Notebooks/DiffustionST_v2/blipdiffusion"
+    model_key = "/BlenderDiffuseNST/DiffuseST_with_TexTile"
     blip_diffusion_pipe = BLIP.from_pretrained(model_key, torch_dtype=torch.float16).to("cuda")
     
     
